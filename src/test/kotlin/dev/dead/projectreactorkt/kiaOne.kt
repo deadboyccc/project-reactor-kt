cfba905e41717a6667b5f31dc4cef1a7be7b3721
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-03-13 10:12:24 +0300

    ch1

---
+++ src/test/kotlin/dev/dead/projectreactorkt/kiaOne.kt
@@ -0,0 +1,11 @@
+package dev.dead.projectreactorkt
+
+import kotlin.test.Test
+import kotlin.test.assertTrue
+
+class kiaOne {
+    @Test
+    fun testTrueDemo() {
+        assertTrue(true)
+    }
+}
