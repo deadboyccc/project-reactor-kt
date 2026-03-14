5ae639a9a08ac2202d0f17c6ad35ebe2d3c85f7d
Ahmed M. Ali <ahmed.mohammed.ali9999@gmail.com>
2026-03-14 11:05:01 +0300

    part 1 kia

--- src/test/kotlin/dev/dead/projectreactorkt/kiaOne.kt
+++ src/test/kotlin/dev/dead/projectreactorkt/kiaOne.kt
@@ -8,4 +8,51 @@ class kiaOne {
     fun testTrueDemo() {
         assertTrue(true)
     }
+
+    @Test
+    fun testWhen() {
+        whenOnInt(0, 1).forEach { print("$it ") }
+
+    }
+
+    fun whenOnInt(a: Int, b: Int): String {
+        return when (setOf(a, b)) {
+            setOf(1, 2) -> "one and two"
+            setOf(0, 1) -> "binary"
+            else -> "default"
+        }
+    }
+
+    sealed interface Expr
+
+    class Num(val value: Int) : Expr
+    class Sum(val left: Expr, val right: Expr) : Expr
+
+    fun eval(e: Expr): Int = when (e) {
+        is Num -> e.value
+        is Sum -> eval(e.left) + eval(e.right)
+    }
+
+    @Test
+    fun testEval() {
+        // Result: (1 + 2) + 4 = 7
+        println(eval(Sum(Sum(Num(1), Num(2)), Num(4))))
+        println(evalWithLogging(Sum(Sum(Num(1), Num(2)), Num(4))))
+    }
+
+    fun evalWithLogging(e: Expr): Int =
+        when (e) {
+            is Num -> {
+                println("num: ${e.value}")
+                e.value
+            }
+
+            is Sum -> {
+                val left = evalWithLogging(e.left)
+                val right = evalWithLogging(e.right)
+                println("sum: $left + $right")
+                left + right
+            }
+
+        }
 }
